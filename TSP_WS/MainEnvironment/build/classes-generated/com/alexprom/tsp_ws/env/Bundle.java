b6f2d2fbf42d185cee43f6a38ba00a102ca3e7fe
yuraprogrammer <yuraprogrammer@users.noreply.github.com>
2022-08-06 16:24:39 +0300

    Каркас проекта

---
+++ TSP_WS/MainEnvironment/build/classes-generated/com/alexprom/tsp_ws/env/Bundle.java
@@ -0,0 +1,69 @@
+package com.alexprom.tsp_ws.env;
+/** Localizable strings for {@link com.alexprom.tsp_ws.env}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Додатково</i>
+     * @see MainOutputTopComponent
+     */
+    static String CTL_MainOutputAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainOutputAction");
+    }
+    /**
+     * @return <i>Додатково</i>
+     * @see MainOutputTopComponent
+     */
+    static String CTL_MainOutputTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainOutputTopComponent");
+    }
+    /**
+     * @return <i>Підробиці</i>
+     * @see MainPropertiesTopComponent
+     */
+    static String CTL_MainPropertiesAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainPropertiesAction");
+    }
+    /**
+     * @return <i>Підробиці</i>
+     * @see MainPropertiesTopComponent
+     */
+    static String CTL_MainPropertiesTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainPropertiesTopComponent");
+    }
+    /**
+     * @return <i>Інформація</i>
+     * @see MainTreeTopComponent
+     */
+    static String CTL_MainTreeAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainTreeAction");
+    }
+    /**
+     * @return <i>Інформація</i>
+     * @see MainTreeTopComponent
+     */
+    static String CTL_MainTreeTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MainTreeTopComponent");
+    }
+    /**
+     * @return <i>This is a MainOutput window</i>
+     * @see MainOutputTopComponent
+     */
+    static String HINT_MainOutputTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_MainOutputTopComponent");
+    }
+    /**
+     * @return <i>This is a MainProperties window</i>
+     * @see MainPropertiesTopComponent
+     */
+    static String HINT_MainPropertiesTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_MainPropertiesTopComponent");
+    }
+    /**
+     * @return <i>This is a MainTree window</i>
+     * @see MainTreeTopComponent
+     */
+    static String HINT_MainTreeTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_MainTreeTopComponent");
+    }
+    private void Bundle() {}
+}
---
+++ TSP_WS/VehicleWeight/build/classes-generated/com/alexprom/tsp_ws/vehicleweight/Bundle.java
@@ -0,0 +1,27 @@
+package com.alexprom.tsp_ws.vehicleweight;
+/** Localizable strings for {@link com.alexprom.tsp_ws.vehicleweight}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Зважування</i>
+     * @see weightingVisTopComponent
+     */
+    static String CTL_weightingVisAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_weightingVisAction");
+    }
+    /**
+     * @return <i>Зважування</i>
+     * @see weightingVisTopComponent
+     */
+    static String CTL_weightingVisTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_weightingVisTopComponent");
+    }
+    /**
+     * @return <i>This is a weightingVis window</i>
+     * @see weightingVisTopComponent
+     */
+    static String HINT_weightingVisTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_weightingVisTopComponent");
+    }
+    private void Bundle() {}
+}
